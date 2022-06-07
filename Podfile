610c4682d20c2fc59d2da24f318891bed146e597
Frank Saar <frank.saar@gmail.com>
2022-06-07 15:27:12 +0100

    initial commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '15.0'
+
+use_frameworks!
+inhibit_all_warnings!
+
+target 'iocApp' do
+end
+
+target 'iocAppTests' do
+    pod 'Quick', '2.2.0'
+    pod 'Nimble', '8.0.5'
+end
---
+++ iocAppTests/iocAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  iocAppTests.swift
+//  iocAppTests
+//
+//  Created by Frank Saar on 07/06/2022.
+//
+
+import XCTest
+@testable import iocApp
+
+class iocAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
